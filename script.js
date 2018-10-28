3471c04de5b934f17cbcc21df2815bd6ad16c7cd
pabushabi <pabushabi@mail.ru>
2018-10-28 18:13:41 +0900

    Added popup article`s viewer, changed buttons on r_bar and more

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ var con_m = document.getElementById("con_menu");
             else {
                 con_m.style.left = e.pageX - 150 + 'px';
             }
-            if ((e.pageY + 110) < document.documentElement.clientHeight){
+            if ((e.pageY + 145) < document.documentElement.clientHeight){
                 con_m.style.top = e.pageY + 'px';
             }
             else {
-                con_m.style.top = e.pageY - 110 + 'px';
+                con_m.style.top = e.pageY - 145 + 'px';
             }
         });
     addEventListener("click",
@@ -37,3 +37,38 @@ var con_m = document.getElementById("con_menu");
             con_m.style.display = "none";
         })
 })();
+
+var scroll_u = document.getElementById("up");
+scroll_u.addEventListener("click", function (e) {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+var scroll_d = document.getElementById("down");
+scroll_d.addEventListener("click", function (e) {
+    window.scrollTo({
+        top: 10000,
+        behavior: "smooth"
+    });
+});
+
+var back = document.getElementById("back");
+back.addEventListener("click", function (e) {
+    history.back();
+});
+
+var big = document.getElementById("big_article");
+var close = document.getElementById("close");
+var article = document.getElementsByClassName("art");
+[].forEach.call(article, function (el) {
+    el.addEventListener("click", function (e) {
+        console.log(e);
+        big.style.display = "block";
+    });
+});
+
+close.addEventListener("click", function (e) {
+    big.style.display = "none";
+});
