f51abca70bf4c27782f64e611a38b15c9b27b4a7
pabushabi <pabushabi@mail.ru>
2018-11-18 22:13:53 +0900

    Added scripted filling of html page + some script improvements

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-"use strict";
+'use strict';
 console.log("Hi^^");
 let fish = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 let menu = document.getElementById("h_menu");
+
+(function () {
+    let section = document.createElement('section');
+    let main = document.getElementById('main');
+    section.className = "sec";
+    main.appendChild(section);
+    for (let i = 0; i < 20; i++){
+        let article = document.createElement("article");
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        article.className = "art";
+        article.id = i+1;
+        h2.id = i+1;
+        p.id = i+1;
+        h2.innerHTML = i+1 + ' article';
+        p.className = 'text';
+        section.appendChild(article);
+        article.appendChild(h2);
+        article.appendChild(p);
+    }
+})();
+
+jQuery.ajax({
+    url: '127.0.0.1:3000',
+    success: function (res) {
+        console.log("success")
+    }
+});
 
 function enMenu() {
     menu.style.display = "block";
@@ -15,8 +43,7 @@ let big_text = document.getElementById("big_b");
 $(function () {
     $(".text").html(fish);
 });
-let texts = document.getElementsByClassName("text");
-$(texts[2]).html(fish + fish);
+
 let con_m = document.getElementById("con_menu");
 (function () {
     big_text.innerHTML = fish;
@@ -69,11 +96,14 @@ let tt = document.getElementById("new_back");
 let big = document.getElementById("big_article");
 let close = document.getElementById("close");
 let article = document.getElementsByClassName("art");
+let big_h = document.getElementById('big_h');
 [].forEach.call(article, function (el) {
     el.addEventListener("click", function (e) {
         console.log(e);
         backgrd.style.display = "block";
         big.style.display = "block";
+        console.log(e.target.id);
+        big_h.innerText = e.target.id + ' article';
     });
 });
 
