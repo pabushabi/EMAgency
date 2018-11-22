549799403daccc559d85a3b81365b1071e9a7dc5
pabushabi <pabushabi@mail.ru>
2018-11-22 14:59:51 +0900

    hotfix

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 console.log("Hi^^");
 let fish = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 let menu = document.getElementById("h_menu");
+let device = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    }
+};
+if (device.Android()){
+    document.getElementById("main").style.marginLeft = 6 + '%';
+    document.getElementById("main").style.marginRight = 6 + '%';
+}
 
 (function () {
     let section = document.createElement('section');
